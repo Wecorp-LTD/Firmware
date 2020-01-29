e0c5ee08ff1d5a857994db112f20b2ff5685e67f
francelico <samuel.garcin@wecorpindustries.com>
2020-01-29 17:43:58 +0000

    feat: changed the maximum values for EKF2_RNG_A_VMAX to 10 and EKF2_RNG_A_HMAX to 60

--- src/modules/ekf2/ekf2_params.c
+++ src/modules/ekf2/ekf2_params.c
@@ -1138,7 +1138,7 @@ PARAM_DEFINE_INT32(EKF2_RNG_AID, 0);
  *
  * @group EKF2
  * @min 0.1
- * @max 2
+ * @max 10
  * @unit m/s
  */
 PARAM_DEFINE_FLOAT(EKF2_RNG_A_VMAX, 1.0f);
@@ -1151,7 +1151,7 @@ PARAM_DEFINE_FLOAT(EKF2_RNG_A_VMAX, 1.0f);
  *
  * @group EKF2
  * @min 1.0
- * @max 10.0
+ * @max 60.0
  * @unit m
  */
 PARAM_DEFINE_FLOAT(EKF2_RNG_A_HMAX, 5.0f);
